54c9b682d3b66c6f2ec20514c9d6a9cae642ee58
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-03-13 23:25:53 +0100

    implement authentication and database with convex and clerk

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
-import { Logo } from "./logo";
+// Components:
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import { Logo } from "./logo";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -19,6 +26,35 @@ export const Navbar = () => {
       <Logo />
 
       <div className="md:ml-auto md:justify-end justify-between flex items-center gap-x-2">
+        {/* Loading State */}
+        {isLoading && <Spinner />}
+
+        {/* Not Authenticated Case */}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button size="sm">Get Notion free</Button>
+            </SignInButton>
+          </>
+        )}
+
+        {/* Authenticated Case */}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm">
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://shining-orca-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
