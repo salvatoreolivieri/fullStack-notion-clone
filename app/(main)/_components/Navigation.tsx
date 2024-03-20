bb8eb8cfef9b0cf007c02abea411b970fa8d749a
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-03-20 17:59:31 +0100

    create userSetting in document page

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -125,7 +126,7 @@ export const Navigation = () => {
         </div>
 
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
 
         <div className="mt-4 ">
